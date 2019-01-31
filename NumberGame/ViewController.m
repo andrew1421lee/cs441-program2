9b27a1f7bbbd060ade02a9af82034497532a94ca
Anchu Lee <etirps@drowmac.local>
2019-01-31 12:38:39 -0500

    new tiles made and animated to appear

--- NumberGame/ViewController.m
+++ NumberGame/ViewController.m
@@ -20,8 +20,22 @@
             cell20, cell21, cell22, cell23,
             cell30, cell31, cell32, cell33;
 
+static NSArray *backgroundCells;
+static NSMutableArray *currentTiles;
+static NSMutableArray *takenCells;
+
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    // Put background cells into an array
+    backgroundCells = [NSArray arrayWithObjects:
+                       cell00, cell01, cell02, cell03,
+                       cell10, cell11, cell12, cell13,
+                       cell20, cell21, cell22, cell23,
+                       cell30, cell31, cell32, cell33, nil];
+    currentTiles = [[NSMutableArray alloc] init];
+    takenCells = [[NSMutableArray alloc] init];
+    
     UISwipeGestureRecognizer *swipeUp = [[UISwipeGestureRecognizer alloc] initWithTarget:self action:@selector(handleSwipe:)];
     swipeUp.direction = UISwipeGestureRecognizerDirectionUp;
     [self.view addGestureRecognizer:swipeUp];
@@ -56,7 +70,85 @@
     if(sender.direction == UISwipeGestureRecognizerDirectionRight) {
         NSLog(@"RIGHT");
     }
+    
+    [self spawnTile];
 }
 
+- (void) gameOver {
+    UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"GAME OVER" message:@"You lost! Too bad" preferredStyle:UIAlertControllerStyleAlert];
+    
+    UIAlertAction *ok = [UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
+        //
+    }];
+    
+    [alert addAction:ok];
+    [self presentViewController:alert animated:YES completion:nil];
+    
+}
+
+// Returns index in backgroundCells that is empty
+- (NSNumber*) getEmptyIndex {
+    bool found = false;
+    bool gameOver = false;
+    NSNumber *index = [NSNumber numberWithUnsignedInteger:arc4random_uniform(16)];
+    
+    while(!found && !gameOver){
+        if([takenCells count] == 16) {
+            NSLog(@"GAME OVER");
+            gameOver = true;
+            break;
+        }
+        
+        if(![takenCells containsObject:(index)]) {
+            found = true;
+        } else {
+            index = [NSNumber numberWithUnsignedInteger:arc4random_uniform(16)];
+        }
+    }
+    
+    if(gameOver)
+    {
+        index = [NSNumber numberWithInt:-1];
+    }
+    
+    return index;
+}
+
+- (void) spawnTile {
+    NSNumber *spawnPosition = [self getEmptyIndex];
+    if([spawnPosition intValue] < 0){
+        [self gameOver];
+        return;
+    }
+    
+    NSLog(@"Spawning tile at: %d", [spawnPosition intValue]);
+    [takenCells addObject:spawnPosition];
+    
+    UILabel *bkgdCell = [backgroundCells objectAtIndex:[spawnPosition intValue]];
+    NSLog(@"%@", [bkgdCell text]);
+    int x = bkgdCell.frame.origin.x;
+    int y = bkgdCell.frame.origin.y;
+    
+    int height = bkgdCell.frame.size.height;
+    int width = bkgdCell.frame.size.width;
+    
+    UILabel *madeLabel = [[UILabel alloc] initWithFrame:CGRectMake(x, y, width, height)];
+    [madeLabel setText:@"test"];
+    [madeLabel setTextAlignment:NSTextAlignmentCenter];
+    [madeLabel setBackgroundColor:[UIColor blueColor]];
+    [madeLabel setAlpha:0.0f];
+    madeLabel.layer.cornerRadius = 8;
+    madeLabel.layer.masksToBounds = TRUE;
+    
+    [UIView animateWithDuration:0.5 animations:^{
+        [madeLabel setAlpha:1.0f];
+    } completion:^(BOOL finished) {
+        //
+    }];
+    
+    [currentTiles addObject:madeLabel];
+    
+    [[self view] addSubview:madeLabel];
+}
 
 @end
